0e0c176442e6bbe3eeeaf9fc113e9121b91c9c88
Madmax <113833416+NANDILLONMaxence@users.noreply.github.com>
2024-08-10 16:10:57 +0200

    Update 001_Install_java-11.0-linux-all.sh
    upgrade code

--- 001_Install_java-11.0-linux-all.sh
+++ 001_Install_java-11.0-linux-all.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
-# Update the package index
-sudo apt update
+# Variable pour la couleur des messages
+color_B="\033[1;34m"  # Bleu clair
+color_R="\033[1;31m"  # Rouge clair
+color_G="\033[1;32m"  # Vert clair
+reset_color="\033[0m"  # Réinitialisation de la couleur
+
+# Fonction pour afficher les messages avec couleur et délai
+show_message() {
+    echo -e "${color_B}$1${reset_color}"
+    sleep 1.5
+}
+
+# Fonction pour afficher les messages d'erreur avec couleur et délai
+error_message() {
+    echo -e "${color_R}$1${reset_color}"
+    sleep 1.5
+}
+
+# Fonction pour afficher les messages avec couleur et délai
+info_message() {
+    echo -e "${color_G}$1${reset_color}"
+    sleep 1.5
+}
 
 # Check if Java is already installed
 if ! command -v java &> /dev/null; then
-    echo "Java is not currently installed."
-    
+    show_message "Java is not currently installed."
+
     # Ask the user if they want to install Java JRE or JDK
-    read -p "Do you want to install Java JRE (1) or Java JDK (2)? Enter 1 or 2 : " java_option
+    show_message "Do you want to install Java JRE (1) or Java JDK (2)? Enter 1 or 2 : "
+    read -p "" java_option
 
     # Install Java JRE
     if [ "$java_option" == "1" ]; then
         sudo apt install -y default-jre
-        java -version
+        info_message "Java jre OK."
+        javac -version
     # Install Java JDK
     elif [ "$java_option" == "2" ]; then
         sudo apt install -y default-jdk
+        info_message "Java jdk OK."
         javac -version
     else
-        echo "Invalid choice. Exiting the script."
+        error_message "Invalid choice. Exiting the script."
         exit 1
     fi
 else
-    echo "Java is already installed:"
+    info_message "Java is already installed:"
     java -version
 fi
